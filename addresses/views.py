548685ea4add6a076402e2ef9c3feacf876851db
unknown <l.albaran@plataform.com>
2025-04-26 16:37:05 -0500

    Implement Address CRUD: Serializer, Views, and URLs. Add initial migration.

--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Address
+from .serializers import AddressSerializer
+ 
+# Vista para crear y listar direcciones
+class AddressListCreateView(generics.ListCreateAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
 
-# Create your views here.
+# Vista para obtener, actualizar y eliminar una dirección especifica
+class AddressRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
