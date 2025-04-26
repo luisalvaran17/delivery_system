128e1b32a9d971093f97d9dc1952015a437e3abd
unknown <l.albaran@plataform.com>
2025-04-26 18:00:21 -0500

    Implement JWT authentication for Address CRUD API

--- drivers/views.py
+++ drivers/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from .models import Driver
 from .serializers import DriverSerializer
+from rest_framework.permissions import IsAuthenticated
  
 # View to create and list drivers
 class DriverListCreateView(generics.ListCreateAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+    permission_classes = [IsAuthenticated]
 
 # View to get, update, and delete a specific driver
 class DriverRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+    permission_classes = [IsAuthenticated]
