f4c271beab70c979ea48bfea1899bcbf3edad22f
unknown <l.albaran@plataform.com>
2025-04-26 15:59:27 -0500

    feat: add Address model

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.street}, {self.city}"
